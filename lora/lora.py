27d6e104fdef600c527e34285fa5003a1bac5c44
Julen <badiolajulen@opendeusto.es>
2020-12-13 13:37:07 +0100

    First commit

---
+++ lora/lora.py
@@ -0,0 +1,34 @@
+import array
+
+from . import rfm as pyrfm
+
+conf = {"ll": {"type": "rfm95"}, "pl": {"type": "serial_seed", "port": "/dev/serial0"}}
+
+
+class LoraEndpoint:
+    def __init__(
+        self, modem="Bw125Cr45Sf128", preamble_length=8, frequency=433, tx_power=13
+    ):
+        self.ll = pyrfm.getLL(conf)
+        if self.ll.setOpModeSleep(True, True):
+            self.ll.setFiFo()
+            self.ll.setOpModeIdle()
+            self.ll.setModemConfig(modem)
+            self.ll.setPreambleLength(preamble_length)
+            self.ll.setFrequency(frequency)
+            self.ll.setTxPower(tx_power)
+
+    def read(self) -> bytes:
+        if self.ll.waitRX():
+            data = self.ll.recv()
+            # header = data[0:4]
+            msg = data[4:]
+            return array.array("B", msg).tostring()
+
+    def write_string(self, text: str) -> None:
+        self.ll.sendStr(text)
+        self.ll.waitPacketSent()
+
+    def write(self, data: bytes) -> None:
+        self.ll.send(data)
+        self.ll.waitPacketSent()
---
+++ server_main.py
@@ -0,0 +1,20 @@
+from lora.lora import LoraEndpoint
+import os
+import json
+
+if __name__ == "__main__":
+    print("==================== SERVER EBIKES ===================== \n")
+    lora = LoraEndpoint()
+    while True:
+        try:
+            print("Waiting to receive a message...")
+            encoded_data = lora.read()
+            print(f"Encoded data received: {encoded_data}")
+            decoded_data = decode_data(encoded_data)
+            print(f"Decoded data: {decoded_data}")
+            time.sleep(10)
+        except:
+            pass
+
+def decode_data(data: bytes) -> str:
+    return data.decode()
