55b6f3d317ad3ecacce2f6b53c3067c1d8ca4893
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2021-01-07 17:25:14 +0100

    Case sensitive git

--- server_main.py
+++ server_main.py
@@ -7,22 +7,22 @@ from logic.Elevator import Elevator
 from logic.CapacityController import initialize as initialize_capacity_controller
 
 from lora.lora import LoraEndpoint
-from func import protocol as prot
+from func.protocol import ELEVATOR_CALL, ELEVATOR_ARRIVED, clean_data
 
 properties = PropertiesManager()
 lora = LoraEndpoint()
 elevator = Elevator(properties, lora)
 
 @threaded
-def thread_listen_to_floors(protocol):
+def thread_listen_to_floors():
     while elevator.overall_status:
         try:
             print("CABIN: Waiting to receive a message...")
-            data = protocol.clean_data(lora.read())
+            data = clean_data(lora.read())
             
-            if protocol.ELEVATOR_CALL in data:
+            if ELEVATOR_CALL in data:
                 print("Received elevator call")
-                calledFloor = int(data[protocol.ELEVATOR_CALL])
+                calledFloor = int(data[ELEVATOR_CALL])
                 elevator.call(calledFloor)
             elevator.send_arrived_lora()
         
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     
     #Initializing
     elevator.call(0)
-    thread_listen_to_floors(prot)
+    thread_listen_to_floors()
     initialize_capacity_controller()
     
 
